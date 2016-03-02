c52ca3caa7d9461098656db80b4088108229c9ac
Joshua Bodine <josh@joshb.us>
2016-03-01 20:44:41 -0700

    Changed standard key mappings to reflect GCW Zero controls.

--- src/bzflag/ActionBinding.cxx
+++ src/bzflag/ActionBinding.cxx
@@ -93,77 +93,29 @@ ActionBinding::ActionBinding() {
   wayToBindActions.insert(std::make_pair(std::string("messagepanel server"), press));
   wayToBindActions.insert(std::make_pair(std::string("messagepanel misc"), press));
 
-  defaultBinding.insert(BindingTable::value_type("F12", "quit"));
-  defaultBinding.insert(BindingTable::value_type("Left Mouse", "fire"));
-  defaultBinding.insert(BindingTable::value_type("Enter", "fire"));
-  defaultBinding.insert(BindingTable::value_type("Middle Mouse", "drop"));
-  defaultBinding.insert(BindingTable::value_type("Space", "drop"));
-  defaultBinding.insert(BindingTable::value_type("Right Mouse", "identify"));
-  defaultBinding.insert(BindingTable::value_type("I", "identify"));
+  defaultBinding.insert(BindingTable::value_type("Enter", "restart"));
+  defaultBinding.insert(BindingTable::value_type("Space", "cycleRadar 0.25 1 0.5"));
+  defaultBinding.insert(BindingTable::value_type("Ctrl+Space", "toggle displayScore"));
+  defaultBinding.insert(BindingTable::value_type("Backspace", "fire"));
+  defaultBinding.insert(BindingTable::value_type("Ctrl+Backspace", "identify"));
   defaultBinding.insert(BindingTable::value_type("Tab", "jump"));
-  defaultBinding.insert(BindingTable::value_type("N", "send all"));
-  defaultBinding.insert(BindingTable::value_type("M", "send team"));
-  defaultBinding.insert(BindingTable::value_type(",", "send nemesis"));
-  defaultBinding.insert(BindingTable::value_type(".", "send recipient"));
-  defaultBinding.insert(BindingTable::value_type("Z", "send admin"));
-  defaultBinding.insert(BindingTable::value_type("S", "toggle displayScore"));
-  defaultBinding.insert(BindingTable::value_type("B", "viewZoom toggle"));
-  defaultBinding.insert(BindingTable::value_type("Ctrl+Wheel Up", "viewZoom in"));
-  defaultBinding.insert(BindingTable::value_type("Ctrl+Wheel Down", "viewZoom out"));
+  defaultBinding.insert(BindingTable::value_type("Ctrl+Tab", "drop"));
   defaultBinding.insert(BindingTable::value_type("Pause", "pause"));
-  defaultBinding.insert(BindingTable::value_type("P", "pause"));
-  defaultBinding.insert(BindingTable::value_type("F1", "fullscreen"));
-  defaultBinding.insert(BindingTable::value_type("Q", "toggleRadar"));
-  defaultBinding.insert(BindingTable::value_type("W", "toggleConsole"));
-  defaultBinding.insert(BindingTable::value_type("F4", "iconify"));
-  defaultBinding.insert(BindingTable::value_type("F5", "screenshot"));
-  defaultBinding.insert(BindingTable::value_type("-", "time backward"));
-  defaultBinding.insert(BindingTable::value_type("=", "time forward"));
-  defaultBinding.insert(BindingTable::value_type("H", "toggleFlags radar"));
-  defaultBinding.insert(BindingTable::value_type("J", "toggleFlags main"));
-  defaultBinding.insert(BindingTable::value_type("K", "silence"));
-  defaultBinding.insert(BindingTable::value_type("L", "toggle displayLabels"));
-  defaultBinding.insert(BindingTable::value_type("Delete", "destruct"));
-
-  // Default movement keys
   defaultBinding.insert(BindingTable::value_type("Left Arrow", "turn left"));
   defaultBinding.insert(BindingTable::value_type("Right Arrow", "turn right"));
   defaultBinding.insert(BindingTable::value_type("Up Arrow", "drive forward"));
   defaultBinding.insert(BindingTable::value_type("Down Arrow", "drive reverse"));
-  // End default movement keys
-
-  defaultBinding.insert(BindingTable::value_type("Shift+Wheel Up", "radarZoom in"));
-  defaultBinding.insert(BindingTable::value_type("Shift+Wheel Down", "radarZoom out"));
-
   defaultBinding.insert(BindingTable::value_type("F6", "roam cycle subject backward"));
   defaultBinding.insert(BindingTable::value_type("F7", "roam cycle subject forward"));
   defaultBinding.insert(BindingTable::value_type("F8", "roam cycle type forward"));
-  defaultBinding.insert(BindingTable::value_type("F9", "roam zoom in"));
-  defaultBinding.insert(BindingTable::value_type("F10", "roam zoom out"));
-  defaultBinding.insert(BindingTable::value_type("F11", "roam zoom normal"));
-  defaultBinding.insert(BindingTable::value_type("O", "servercommand"));
-  defaultBinding.insert(BindingTable::value_type("F", "toggle displayFlagHelp"));
   defaultBinding.insert(BindingTable::value_type("Page Up", "scrollpanel up_page"));
-  defaultBinding.insert(BindingTable::value_type("Wheel Up", "scrollpanel up 3"));
   defaultBinding.insert(BindingTable::value_type("Page Down", "scrollpanel down_page"));
-  defaultBinding.insert(BindingTable::value_type("Wheel Down", "scrollpanel down 3"));
-  defaultBinding.insert(BindingTable::value_type("End", "scrollpanel bottom"));
-  defaultBinding.insert(BindingTable::value_type("1", "set displayRadarRange 0.25"));
-  defaultBinding.insert(BindingTable::value_type("2", "set displayRadarRange 0.5"));
-  defaultBinding.insert(BindingTable::value_type("3", "set displayRadarRange 1.0"));
-  defaultBinding.insert(BindingTable::value_type("4", "radarZoom in"));
-  defaultBinding.insert(BindingTable::value_type("5", "radarZoom out"));
-  defaultBinding.insert(BindingTable::value_type("A", "toggle slowKeyboard"));
-  defaultBinding.insert(BindingTable::value_type("U", "hunt"));
-  defaultBinding.insert(BindingTable::value_type("7", "addhunt"));
-  defaultBinding.insert(BindingTable::value_type("Right Mouse", "restart"));
-  defaultBinding.insert(BindingTable::value_type("I", "restart"));
-  defaultBinding.insert(BindingTable::value_type("9", "autopilot"));
-
-  defaultBinding.insert(BindingTable::value_type("Shift+F1", "messagepanel all"));
-  defaultBinding.insert(BindingTable::value_type("Shift+F2", "messagepanel chat"));
-  defaultBinding.insert(BindingTable::value_type("Shift+F3", "messagepanel server"));
-  defaultBinding.insert(BindingTable::value_type("Shift+F4", "messagepanel misc"));
+  defaultBinding.insert(BindingTable::value_type("A", "viewZoom toggle"));
+  defaultBinding.insert(BindingTable::value_type("Ctrl+A", "toggle slowKeyboard"));
+  defaultBinding.insert(BindingTable::value_type("L", "cyclePanel left_off"));
+  defaultBinding.insert(BindingTable::value_type("R", "cyclePanel right_off"));
+  defaultBinding.insert(BindingTable::value_type("X", "pause"));
+  defaultBinding.insert(BindingTable::value_type("Ctrl+X", "destruct"));
 }
 
 void ActionBinding::resetBindings() {
