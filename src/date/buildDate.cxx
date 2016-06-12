86ab0fa4e2616fe59d2dc5d9feff2a06742cea44
Joshua Bodine <josh@joshb.us>
2016-06-12 13:55:05 -0700

    Change the GCW Zero protocol prefix to one that actually works with the list server.

--- src/date/buildDate.cxx
+++ src/date/buildDate.cxx
@@ -109,7 +109,7 @@ const char*		getProtocolVersion()
 const char*		getServerVersion()
 {
   static std::string serverVersion = ""; // no idea why, but we can't pull the server list unless this is declared static (even though it's now dynamic)...
-  serverVersion = std::string(BZDB.isSet("desktopServerList") && BZDB.evalInt("desktopServerList") ? "BZFS" : "GCW0") + getProtocolVersion();
+  serverVersion = std::string(BZDB.isSet("desktopServerList") && BZDB.evalInt("desktopServerList") ? "BZFS" : "GCWZ") + getProtocolVersion();
   return serverVersion.c_str();
 }
 
